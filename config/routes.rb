0d93af310dff99ec39428cb057353feda5ff3ea3
Max Kadel <mkadel@princeton.edu>
2023-09-20 15:20:50 -0400

    Add basic info to root of app
    Closes #77

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'main#index', defaults: { format: 'json' }
   get '/search/artmuseum/', to: 'art_museum#show', defaults: { format: 'json' }
   get '/search/catalog/', to: 'catalog#show', defaults: { format: 'json' }
   get '/search/dpul/', to: 'dpul#show', defaults: { format: 'json' }
---
+++ spec/requests/main_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'GET /' do
+  it 'renders json' do
+    get '/'
+    expect(response).to be_successful
+    expect(response.content_type).to eq('application/json; charset=utf-8')
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    expect(response_body[:application]).to eq('Princeton University Library All Search API')
+    expect(response_body[:environment]).to eq('test')
+    expect(response_body[:github_link]).to eq('https://github.com/pulibrary/allsearch_api')
+  end
+end
