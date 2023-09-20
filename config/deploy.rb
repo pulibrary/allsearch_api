1ee66db428b451c088d454bf329e9fbce15cf16c
Max Kadel <mkadel@princeton.edu>
2023-09-20 15:53:47 -0400

    Rename github repo (#92)

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-set :repo_url, 'https://github.com/pulibrary/allsearch_rails_api.git'
-set :application, 'allsearch_rails_api'
+set :repo_url, 'https://github.com/pulibrary/allsearch_api.git'
+set :application, 'allsearch_api'
 
 set :branch, ENV['BRANCH'] || 'main'
 
@@ -18,7 +18,7 @@ namespace :passenger do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute 'passenger-config restart-app /opt/allsearch_rails_api/current'
+      execute 'passenger-config restart-app /opt/allsearch_api/current'
     end
   end
 end
