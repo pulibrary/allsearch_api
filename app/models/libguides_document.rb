064bd9a3c2498f12b8b71c901ca766ae07a5a2b2
Jane Sandberg <js7389@princeton.edu>
2023-09-21 14:46:09 -0700

    i29: Libguides service

---
+++ app/models/libguides_document.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class LibguidesDocument < Document
+  def title
+    document['name']
+  end
+
+  def creator
+    return if guide_owner.blank?
+
+    "#{guide_owner['first_name']} #{guide_owner['last_name']}"
+  end
+
+  def publisher
+    'Princeton University Library'
+  end
+
+  def id
+    document['id'].to_s
+  end
+
+  def type
+    document['type_label']
+  end
+
+  def description
+    document['description']
+  end
+
+  def url
+    document['friendly_url'] || document['url']
+  end
+
+  def doc_keys
+    []
+  end
+
+  private
+
+  def guide_owner
+    @guide_owner ||= document['owner']
+  end
+end
