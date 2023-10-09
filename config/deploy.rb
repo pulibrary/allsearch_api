25c371406ca2ca53a87e174c6b5a82a8b59dd28d
Max Kadel <mkadel@princeton.edu>
2023-10-09 10:35:11 -0400

    Try to align cap / passenger with other apps

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,21 +6,9 @@ set :application, 'allsearch_api'
 set :branch, ENV['BRANCH'] || 'main'
 
 set :deploy_to, -> { "/opt/#{fetch(:application)}" }
-set :repo_path, -> { "/opt/#{fetch(:application)}/repo" }
 
 set :log_level, :debug
 
 set :ssh_options, { forward_agent: true }
 
 set :linked_dirs, %w[log]
-
-namespace :passenger do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute 'passenger-config restart-app /opt/allsearch_api/current'
-    end
-  end
-end
-
-after 'deploy:publishing', 'passenger:restart'
