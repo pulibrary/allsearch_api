8f9c933f8283ddcb38ab705ad6e7ba80d0d9dd29
Jane Sandberg <sandbergja@users.noreply.github.com>
2024-03-04 15:06:50 -0800

    Point catalog staging to solr8d cluster (#204)

--- spec/models/catalog_spec.rb
+++ spec/models/catalog_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Catalog do
     context 'when on a non-production environment' do
       before do
         allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('staging'))
-        stub_request(:get, 'http://lib-solr8-staging.princeton.edu:8983/solr/catalog-staging/select?facet=false&fl=id,title_display,author_display,pub_created_display,format,holdings_1display,electronic_portfolio_s,electronic_access_1display&q=rubix&rows=3&sort=score%20desc,%20pub_date_start_sort%20desc,%20title_sort%20asc')
+        stub_request(:get, 'http://lib-solr8d-staging.princeton.edu:8983/solr/catalog-staging/select?facet=false&fl=id,title_display,author_display,pub_created_display,format,holdings_1display,electronic_portfolio_s,electronic_access_1display&q=rubix&rows=3&sort=score%20desc,%20pub_date_start_sort%20desc,%20title_sort%20asc')
           .to_return(status: 200, body: file_fixture('solr/catalog/rubix.json'))
       end
 
