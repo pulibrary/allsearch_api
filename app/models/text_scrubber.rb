5ca10ced150dafbd6dcea6221e5c9c115942caea
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-07-08 09:48:33 -0400

    Rubocop errors

--- app/models/text_scrubber.rb
+++ app/models/text_scrubber.rb
@@ -4,7 +4,7 @@ class TextScrubber < Rails::HTML::PermitScrubber
   def initialize
     super
     # List of allowed HTML tags
-    self.tags = %w( b em i p ul ol li h1 h2 h3 h4 h5 h6 )
+    self.tags = %w[b em i p ul ol li h1 h2 h3 h4 h5 h6]
   end
 
   def skip_node?(node)
