7c1fd68f1d8102b51b0481ed856ebdb58e819463
Ryan Laddusaw <rladdusaw@princeton.edu>
2024-07-15 13:07:55 -0400

    actually replace and downcase url

--- app/models/library_staff_document.rb
+++ app/models/library_staff_document.rb
@@ -82,9 +82,9 @@ class LibraryStaffDocument < Document
   def description; end
 
   def url
-    staff_url = "https://library.psb-prod.princeton.edu/people/#{document.first_name}-#{document.last_name}"
-    staff_url.sub(' ', '-')
-    staff_url.downcase
+    "https://library.psb-prod.princeton.edu/people/#{document.first_name}-#{document.last_name}"
+      .gsub(' ', '-')
+      .downcase
   end
 
   def doc_keys
