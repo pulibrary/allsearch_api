723d4b9c479758115c5b0b31848cf387560a0ecb
Max Kadel <mkadel@princeton.edu>
2024-07-21 16:25:54 -0400

    Try adding checks for multiple solrs
    Reek doesn't like this right now, and I'm not sure if it's premature optimization

--- app/checks/solr_status.rb
+++ app/checks/solr_status.rb
@@ -2,21 +2,29 @@
 
 class SolrStatus < HealthMonitor::Providers::Base
   def check!
-    response = Net::HTTP.get_response(status_uri)
-    json = JSON.parse(response.body)
-    raise "The solr has an invalid status #{status_uri}" if json['responseHeader']['status'] != 0
+    failed_solrs = []
+    status_uris.each do |status_uri|
+      response = Net::HTTP.get_response(status_uri)
+      json = JSON.parse(response.body)
+      success = json['responseHeader']['status'].zero?
+      failed_solrs << status_uri unless success
+    end
+    raise "The solr has an invalid status #{failed_solrs.join(', ')}" unless failed_solrs.empty?
   end
 
-  # We will want to be able to check both solr8 and solr9 cores
-  # Right now we're checking "whatever dpul's host is"
-  def solr_config
-    Rails.application.config_for(:allsearch)[:dpul][:solr]
+  # Right now all of our services are on solr 8,
+  # but eventually they will be split between solr 8 and solr 9
+  def solr_services_configs
+    Rails.application.config_for(:allsearch).select { |_k, v| v.keys.include?(:solr) }
   end
 
-  def status_uri
-    URI::HTTP.build(host: solr_config[:host],
-                    port: solr_config[:port],
-                    path: '/solr/admin/cores',
-                    query: 'action=STATUS')
+  def status_uris
+    solr_services_configs.map do |_name, config|
+      solr_config = config[:solr]
+      URI::HTTP.build(host: solr_config[:host],
+                      port: solr_config[:port],
+                      path: '/solr/admin/cores',
+                      query: 'action=STATUS')
+    end.uniq.compact
   end
 end
--- spec/checks/solr_status_spec.rb
+++ spec/checks/solr_status_spec.rb
@@ -4,28 +4,55 @@ require 'rails_helper'
 
 RSpec.describe SolrStatus do
   let(:solr_status) { described_class.new }
+  let(:stub_success) do
+    stub_request(:get, 'http://lib-solr8-prod.princeton.edu:8983/solr/admin/cores?action=STATUS').to_return(
+      body: { responseHeader: { status: 0 } }.to_json, headers: { 'Content-Type' => 'text/json' }
+    )
+  end
+  let(:stub_failure) do
+    stub_request(:get, 'http://lib-solr8-prod.princeton.edu:8983/solr/admin/cores?action=STATUS').to_return(
+      body: { responseHeader: { status: 500 } }.to_json, headers: { 'Content-Type' => 'text/json' }
+    )
+  end
 
   context 'with a successful request to solr' do
     before do
-      stub_request(:get, 'http://lib-solr8-prod.princeton.edu:8983/solr/admin/cores?action=STATUS').to_return(
-        body: { responseHeader: { status: 0 } }.to_json, headers: { 'Content-Type' => 'text/json' }
-      )
+      stub_success
     end
 
     it 'returns nil on the check! method' do
       expect(solr_status.check!).to be_nil
+      assert_requested(stub_success)
     end
   end
 
   context 'with an unsuccessful request to solr' do
     before do
-      stub_request(:get, 'http://lib-solr8-prod.princeton.edu:8983/solr/admin/cores?action=STATUS').to_return(
-        body: { responseHeader: { status: 500 } }.to_json, headers: { 'Content-Type' => 'text/json' }
-      )
+      stub_failure
     end
 
     it 'raises an error' do
       expect { solr_status.check! }.to raise_error(RuntimeError, /The solr has an invalid status/)
+      assert_requested(stub_failure)
+    end
+  end
+
+  context 'with multiple solr hosts' do
+    let(:stub_solr_9_success) do
+      stub_request(:get, 'http://lib-solr9-prod.princeton.edu:8983/solr/admin/cores?action=STATUS').to_return(
+        body: { responseHeader: { status: 0 } }.to_json, headers: { 'Content-Type' => 'text/json' }
+      )
+    end
+
+    before do
+      stub_success
+      stub_solr_9_success
+    end
+
+    it 'checks more than one solr host' do
+      solr_status.check!
+      assert_requested(stub_success)
+      # assert_requested(stub_solr_9_success)
     end
   end
 end
