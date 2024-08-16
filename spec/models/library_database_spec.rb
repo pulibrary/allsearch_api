05842396319c945a5236de646acc5238841aab28
Jane Sandberg <js7389@princeton.edu>
2024-08-16 10:29:45 -0700

    [LibraryDatabases] Handle decomposed diacritics in more_link
    The rails transliterate method handles precomposed diacritics (when a
    single character is used to represent a letter and its diacritic) just
    fine, but when it finds decomposed diacritics (when a separate character
    is used for the character and another for its diacritic), it converts
    the diacritic into a question mark.
    
    Instead, this converts all diacritics to the decomposed version, then
    deletes the diacritics.

--- spec/models/library_database_spec.rb
+++ spec/models/library_database_spec.rb
@@ -30,5 +30,13 @@ RSpec.describe LibraryDatabase do
     it 'builds a working more_link' do
       expect(db_service.more_link.to_s).to eq('https://libguides.princeton.edu/az/databases?q=Kobunso%20Taika%20Koshomoku')
     end
+
+    context 'when the diacritics are decomposed into two separate characters' do
+      let(:query_terms) { 'Ko%CC%84bunso%CC%84%20Taika%20Koshomoku' }
+
+      it 'builds a working more_link' do
+        expect(db_service.more_link.to_s).to eq('https://libguides.princeton.edu/az/databases?q=Kobunso%20Taika%20Koshomoku')
+      end
+    end
   end
 end
