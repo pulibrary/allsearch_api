ad87f196996a8e20fd6e23fdd345a15511e4d65e
Jane Sandberg <js7389@princeton.edu>
2025-06-05 11:55:20 -0700

    Use semantic_logger instead of lograge
    Co-authored-by: Angel Ruiz <aruiz1789@users.noreply.github.com>
    Co-authored-by: Beck Davis <beck-davis@users.noreply.github.com>
    Co-authored-by: Francis Kayiwa <kayiwa@users.noreply.github.com>
    Co-authored-by: Vickie Karasic <vickiekarasic@users.noreply.github.com>

--- config/initializers/lograge.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.configure do
-  config.lograge.enabled = true
-
-  # We are asking here to log in RAW (which are actually ruby hashes).
-  # The Ruby logging is going to take care of the JSON formatting.
-  config.lograge.formatter = Lograge::Formatters::Logstash.new
-end
