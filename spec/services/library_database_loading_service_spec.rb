9e89193153f13fb78089d7931d9078b8dba93eee
Jane Sandberg <js7389@princeton.edu>
2025-12-01 15:10:38 -0800

    Remove ActiveRecord LibraryDatabaseRecord from a test file
    Helps with #402

--- spec/services/library_database_loading_service_spec.rb
+++ spec/services/library_database_loading_service_spec.rb
@@ -11,51 +11,66 @@ RSpec.describe LibraryDatabaseLoadingService do
   end
 
   it 'creates a new row in the library_database_records table for each CSV row' do
-    expect { described_class.new.run }.to change(LibraryDatabaseRecord, :count).by(14)
-    expect(LibraryDatabaseRecord.third.name).to eq('Abzu')
-    expect(LibraryDatabaseRecord.third.description).to eq('Database of networked open access data relevant to the ' \
-                                                          'study and public presentation of the Ancient Near East ' \
-                                                          'and the Ancient Mediterranean world.')
-    expect(LibraryDatabaseRecord.third.url).to eq('http://www.etana.org/abzubib')
-    expect(LibraryDatabaseRecord.third.subjects).to be_empty
+    rom = Rails.application.config.rom
+    expect { described_class.new.run }.to change { rom.relations[:library_database_records].count }.by(14)
+    third_record = rom.relations[:library_database_records].to_a[2]
+    expect(third_record[:name]).to eq('Abzu')
+    expect(third_record[:description]).to eq('Database of networked open access data relevant to the ' \
+                                             'study and public presentation of the Ancient Near East ' \
+                                             'and the Ancient Mediterranean world.')
+    expect(third_record[:url]).to eq('http://www.etana.org/abzubib')
+    expect(third_record[:subjects]).to be_empty
   end
 
   it 'is idempotent' do
+    rom = Rails.application.config.rom
     described_class.new.run
-    expect { described_class.new.run }.not_to change(LibraryDatabaseRecord, :count)
+    expect { described_class.new.run }.not_to(change { rom.relations[:library_database_records].count })
   end
 
   context 'when file does not have the required headers' do
     let(:libjobs_response) { 'bad response' }
 
     it 'does not proceed' do
-      LibraryDatabaseRecord.create(libguides_id: 123, name: 'JSTOR')
-      expect { described_class.new.run }.not_to(change(LibraryDatabaseRecord, :count))
+      rom = Rails.application.config.rom
+      repo = LibraryDatabaseRepository.new(rom)
+      repo.create(libguides_id: 123, name: 'JSTOR')
+      expect { described_class.new.run }.not_to(change { rom.relations[:library_database_records].count })
     end
   end
 
   context 'when a library database in postgres is no longer in the CSV' do
     it 'removes it from the database' do
-      old_record = LibraryDatabaseRecord.create(libguides_id: 123, name: 'JSTOR')
-      expect(LibraryDatabaseRecord.where(libguides_id: 123)).to contain_exactly(old_record)
+      rom = Rails.application.config.rom
+      repo = LibraryDatabaseRepository.new(rom)
+      repo.create(libguides_id: 123, name: 'JSTOR')
+      expect(rom.relations[:library_database_records].where(libguides_id: 123).count).to eq 1
       described_class.new.run
-      expect(LibraryDatabaseRecord.where(libguides_id: 123)).to be_empty
+      expect(rom.relations[:library_database_records].where(libguides_id: 123).to_a).to be_empty
     end
   end
 
   context 'when a library database has updated info in the CSV' do
     it 'updates the relevant fields' do
-      LibraryDatabaseRecord.create(libguides_id: 2_938_694, name: 'JSTOR')
-      expect(LibraryDatabaseRecord.find_by(libguides_id: 2_938_694).name).to eq 'JSTOR'
+      rom = Rails.application.config.rom
+      repo = LibraryDatabaseRepository.new(rom)
+      repo.create(libguides_id: 2_938_694, name: 'JSTOR')
+      expect(
+        rom.relations[:library_database_records].where(libguides_id: 2_938_694).first[:name]
+      ).to eq 'JSTOR'
       described_class.new.run
-      expect(LibraryDatabaseRecord.find_by(libguides_id: 2_938_694).name).to eq 'AAPG Datapages'
+      expect(
+        rom.relations[:library_database_records].where(libguides_id: 2_938_694).first[:name]
+      ).to eq 'AAPG Datapages'
     end
   end
 
   context 'when the CSV is suspiciously small relative to the number of database rows' do
     it 'does not proceed' do
-      30.times { |number| LibraryDatabaseRecord.create(libguides_id: number, name: 'JSTOR') }
-      expect { described_class.new.run }.not_to(change(LibraryDatabaseRecord, :count))
+      rom = Rails.application.config.rom
+      repo = LibraryDatabaseRepository.new(rom)
+      30.times { |number| repo.create(libguides_id: number, name: 'JSTOR') }
+      expect { described_class.new.run }.not_to(change { rom.relations[:library_database_records].count })
     end
   end
 end
