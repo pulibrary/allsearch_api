2ea5cfbcaa233e0721ab1551960ea56ec0b793a4
Christina Chortaria <actspatial@gmail.com>
2025-12-09 11:13:41 -0500

    initialize rails before accessing Rails.application.config.rom = container
    error is: undefined method 'rom' for an instance of Rails::Application::Configuration'

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require_relative '../config/environment'
 require_relative 'support/database_cleaner'
 
 # Prevent database truncation if the environment is production
