47177f099337b4a8e1ca749dd06abaa89ed8e145
Jane Sandberg <js7389@princeton.edu>
2025-12-12 19:35:52 -0800

    Skip nokogiri-based sanitization for strings that don't contain angle brackets
    It can be expensive to run the full sanitization routine on long
    strings, so let's skip it if they don't need HTML-parser based sanitization,
    using the heuristic of checking for an opening angle bracket.
    
    Also, use ruby's squeeze method to avoid an expensive regular expression.
    
    According to the attached microbenchmark:
    
    before:
    ```
    Warming up --------------------------------------
               sanitizer   583.000 i/100ms
    Calculating -------------------------------------
               sanitizer      6.308k (± 4.5%) i/s  (158.54 μs/i) -     31.482k in   5.001876s
    ```
    
    after:
    ```
    Warming up --------------------------------------
               sanitizer    25.400k i/100ms
    Calculating -------------------------------------
               sanitizer    257.972k (± 3.5%) i/s    (3.88 μs/i) -      1.295M in   5.028767s
    ```

--- app/models/sanitizer.rb
+++ app/models/sanitizer.rb
@@ -5,12 +5,27 @@ require 'cgi'
 
 class Sanitizer
   def sanitize(html)
-    sanitized = sanitizer_implementation.fragment(html)
-    with_fixed_whitespace = sanitized.strip.gsub(/(?:&nbsp;| )+/, ' ')
-    CGI.unescapeHTML with_fixed_whitespace
+    sanitized = remove_problematic_html(html)
+      .gsub('&nbsp;', ' ')
+      .squeeze(' ')
+      .strip
+    CGI.unescapeHTML(sanitized)
   end
 
   private
+
+  def remove_problematic_html(raw)
+    html = raw.to_s
+    if html.include? '<'
+      # We CGI.unescapeHTML before parsing the HTML to make sure
+      # that the original string can't sneak in, say,
+      # a malicious <script> as &lt;script&gt;
+      sanitizer_implementation.fragment(CGI.unescapeHTML(html))
+    else
+      # If there is not an angle bracket, no need to parse it as HTML
+      html
+    end
+  end
 
   def sanitizer_implementation
     @sanitizer_implementation ||= Sanitize.new(elements: %w[b em i strong])
