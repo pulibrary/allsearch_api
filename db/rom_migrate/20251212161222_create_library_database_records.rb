f909f32757aedbc16f84b8104a11221405284d4e
Jane Sandberg <js7389@princeton.edu>
2025-12-16 14:02:22 -0800

    Update syntax for database records searchable column

--- db/rom_migrate/20251212161222_create_library_database_records.rb
+++ db/rom_migrate/20251212161222_create_library_database_records.rb
@@ -15,13 +15,13 @@ Sequel.migration do
       DateTime :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
       DateTime :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
 
-      virtual :searchable,
-              type: :tsvector,
-              as: "setweight(to_tsvector('english', coalesce(name,'')), 'A')    || " \
+      tsvector :searchable,
+              generated_always_as: Sequel.lit(
+                    "setweight(to_tsvector('english', coalesce(name,'')), 'A')    || " \
                     "setweight(to_tsvector('english', coalesce(alt_names_concat,'')), 'B')  || " \
                     "setweight(to_tsvector('english', coalesce(description,'')), 'C') || " \
                     "setweight(to_tsvector('english', coalesce(subjects_concat,'')), 'D') ",
-              stored: true
+              )
     end
     add_index :library_database_records, :searchable,
               using: :gin, name: 'searchable_idx'
