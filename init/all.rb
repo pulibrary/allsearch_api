41d91b723fa44d713c4b27ab9c760ea2f6ac94b1
Jane Sandberg <js7389@princeton.edu>
2025-12-18 05:39:24 -0800

    Move datadog initialization to the init directory
    Helps with #473

--- init/all.rb
+++ init/all.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
+# Fundamental things, should be started as early as possible in the initialization
 require_relative '../app/paths'
-
 require allsearch_path 'init/environment'
-require allsearch_path 'init/honeybadger'
 require allsearch_path 'init/logger'
+
+# Standard init code that does not need to be at the start or end
+require allsearch_path 'init/datadog'
+require allsearch_path 'init/honeybadger'
 require allsearch_path 'init/rom_factory'
 
+# These should be started near the end of the init process
 require allsearch_path 'init/yjit'
