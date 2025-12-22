8e0cb167ac165923c238009800c0b3ccdabc4070
Ryan Laddusaw <rladdusaw@princeton.edu>
2025-12-22 16:49:56 -0500

    Remove the activerecord and activemodel gems
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -3,8 +3,6 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-gem 'activemodel'
-gem 'activerecord'
 gem 'bootsnap', require: false
 gem 'csv'
 gem 'datadog', '~> 2.0'
--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,9 @@
 
 require_relative 'boot'
 
-require 'active_record'
-
 require_relative 'lando_env'
 require_relative 'db_connection'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*CURRENT_ENVIRONMENT.bundler_groups)
-
-# Use the sql format so that we can capture our postgres custom search configuration
-ActiveRecord.schema_format = :sql
