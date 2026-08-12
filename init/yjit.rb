ffa4c9fec22192f87f67e6a3964ceac0d0ab81a1
Ernesto Tagwerker <ernesto+github@ombulabs.com>
2026-08-12 14:28:53 -0400

    Bump version to Ruby 3.4.10

--- init/yjit.rb
+++ init/yjit.rb
@@ -2,4 +2,4 @@
 
 # This should be near the end of init process, so we don't needlessly
 # compile the startup code that we won't use again
-RubyVM::YJIT.enable
+RubyVM::YJIT.enable if defined?(RubyVM::YJIT)
